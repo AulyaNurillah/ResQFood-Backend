c729c9981ad96b87f2efd2a4e7ab2af97f305ee1
Dina ZW <dinawiliansyah3012@gmail.com>
2026-06-14 15:18:40 +0700

    Update Supabase key environment variable to use SUPABASE_ROLE_KEY

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -5,7 +5,7 @@ const WebSocket = require('ws');
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_ROLE_KEY;
 
 let supabase = null;
 
